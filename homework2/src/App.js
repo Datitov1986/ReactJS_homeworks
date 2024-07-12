7e6d70c165e888389a4a1b25b53b1ff978b7ea88
Dmitri <datitov2009@yandex.ru>
2024-07-12 18:03:12 +0200

    add homework2 folder

---
+++ homework2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CommentsList from './components/CommentsList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Мое приложение для домашнего задания</h1>
+      <CommentsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ homework2/src/Message.js
@@ -0,0 +1,10 @@
+import './Message.css'
+function Message({text}) {
+    return ( 
+        <div className="message">
+            <p>{text}</p>
+        </div>
+     );
+}
+
+export default Message;
