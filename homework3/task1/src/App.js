eedf6c0b5f6160fc13c66bf09ecb8ab0c28eaabc
Dmitri <datitov2009@yandex.ru>
2024-07-12 18:23:27 +0200

    add homework3 folder

---
+++ homework3/task1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TemperatureConverter from './components/TemperatureConverter';
+
+
+const App = () => {
+  return (
+    <div>
+      <TemperatureConverter />{" "}
+    </div>
+  );
+};
+
+export default App;
