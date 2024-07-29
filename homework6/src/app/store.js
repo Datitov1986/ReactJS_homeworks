45c5bcf6a8d83033b0011aba5be6068e8dc11f59
Dmitri <datitov2009@yandex.ru>
2024-07-29 13:40:46 +0200

    add homework 6 folder

---
+++ homework6/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "../products/productSlice";
+
+export const store = configureStore({
+    reducer: {
+        products : productReducer,
+    },
+});
+
+export default store;
